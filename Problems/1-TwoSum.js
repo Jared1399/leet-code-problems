6d6364436c0b6ef5acb89eb2405ceaf9f339cf52
yarem.quiero13 <yarem.quiero13@gmail.com>
2023-03-12 17:33:59 -0300

    solved 1-TwoSum

--- Problems/1-TwoSum.js
+++ Problems/1-TwoSum.js
@@ -3,3 +3,28 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    for (let i = 0; i< nums.length; i++){
+        for(let j=0; j<nums.length; j++){
+            
+
+            if(i == j){
+                continue
+            }
+
+            if(nums[i]+nums[j] == target){
+                return [i, j]
+            }
+        }
+    }
+
+};
+
+console.log(twoSum([2,7,11,15],9))
